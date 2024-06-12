7eea9f424b66f797ce9b638ae8d41ca6847b318b
Jarred Parr <jparr721@gmail.com>
2024-06-12 10:23:59 -0600

    fix bug where other folders caused a crash, add human readable sizes

--- crates/jls/src/main.rs
+++ crates/jls/src/main.rs
@@ -8,8 +8,15 @@ use users::{get_group_by_gid, get_user_by_uid};
 #[cfg(unix)]
 use std::os::unix::fs::PermissionsExt;
 
+#[cfg(unix)]
+const SIZES: [&str; 6] = ["", "K", "M", "G", "T", "P"];
+
 #[derive(Debug, Parser)]
+#[clap(disable_help_flag = true)]
 struct Args {
+    #[clap(long, action = clap::ArgAction::HelpLong)]
+    help: Option<bool>,
+
     /// Show files starting with '.'
     #[clap(short, long, default_value_t = false)]
     all: bool,
@@ -17,6 +24,10 @@ struct Args {
     /// Show files in a list format
     #[clap(short, long, default_value_t = false)]
     list: bool,
+
+    /// Show the data in human-readable format
+    #[clap(short, long, default_value_t = false)]
+    human: bool,
 
     /// The path to list
     #[clap(default_value = ".")]
@@ -73,6 +84,24 @@ fn get_size_bytes(metadata: &fs::Metadata) -> String {
     metadata.size().to_string()
 }
 
+#[cfg(unix)]
+fn get_size_human_readable(metadata: &fs::Metadata) -> String {
+    let bytes = metadata.size();
+    if bytes == 0 {
+        return "0".to_string();
+    }
+
+    // If we just have some absolutely ridiculous number, just do bytes
+    let magnitude = bytes.ilog(1024) as usize;
+    let adjusted = (bytes as f64) / (1u64 << (magnitude * 10)) as f64;
+
+    if magnitude > SIZES.len() || magnitude == 0 {
+        format!("{:.1}", adjusted)
+    } else {
+        format!("{:.1}{}", adjusted, SIZES[magnitude])
+    }
+}
+
 #[cfg(not(unix))]
 fn get_metadata(path: &String) -> Result<String> {
     Ok("".to_string())
@@ -86,36 +115,37 @@ fn ls(args: Args) -> Result<String> {
         return Ok(path);
     }
 
-    let mut entries = {
-        let all_files = fs::read_dir(path)?
-            .map(|entry| entry.unwrap().file_name().into_string().unwrap());
+    let mut entries_paths = {
+        let all_files = fs::read_dir(path)?.map(|entry| entry.unwrap().path());
 
-        if !args.all {
-            all_files
-                .filter(|filename| !filename.starts_with('.'))
-                .collect::<Vec<String>>()
+        if args.all {
+            all_files.collect::<Vec<_>>()
         } else {
-            all_files.collect::<Vec<String>>()
+            all_files
+                .filter(|path| {
+                    path.file_name()
+                        .and_then(|name| name.to_str())
+                        .map_or(false, |name| !name.starts_with('.'))
+                })
+                .collect::<Vec<_>>()
         }
     };
 
-    entries.sort();
+    entries_paths.sort();
+    let entries_strs = entries_paths
+        .iter()
+        .map(|e| e.to_str().unwrap().to_string())
+        .collect::<Vec<String>>();
 
     if args.list {
         #[cfg(unix)]
         {
-            let metadatas = entries
+            let metadatas = entries_paths
                 .iter()
                 .map(|entry| fs::metadata(entry).unwrap())
                 .collect::<Vec<fs::Metadata>>();
-            let modes = metadatas
-                .iter()
-                .map(get_mode)
-                .collect::<Vec<String>>();
-            let nlinks = metadatas
-                .iter()
-                .map(get_nlink)
-                .collect::<Vec<String>>();
+            let modes = metadatas.iter().map(get_mode).collect::<Vec<String>>();
+            let nlinks = metadatas.iter().map(get_nlink).collect::<Vec<String>>();
             let owners = metadatas
                 .iter()
                 .map(|metadata| get_file_owner(metadata).unwrap())
@@ -126,7 +156,13 @@ fn ls(args: Args) -> Result<String> {
                 .collect::<Vec<String>>();
             let sizes = metadatas
                 .iter()
-                .map(get_size_bytes)
+                .map(|m| {
+                    if args.human {
+                        get_size_human_readable(m)
+                    } else {
+                        get_size_bytes(m)
+                    }
+                })
                 .collect::<Vec<String>>();
             let last_modified = metadatas
                 .iter()
@@ -138,13 +174,13 @@ fn ls(args: Args) -> Result<String> {
             let max_group_width = groups.iter().map(|group| group.len()).max().unwrap_or(0);
             let max_size_width = sizes.iter().map(|size| size.len()).max().unwrap_or(0);
 
-            Ok(entries
+            Ok(entries_paths
                 .into_iter()
                 .enumerate()
                 .map(|(i, entry)| {
                     format!("{:<10} {:>width$} {:<owner_width$} {:<group_width$} {:>size_width$} {} {entry}",
                             modes[i], nlinks[i], owners[i], groups[i], sizes[i], last_modified[i],
-                            entry = entry,
+                            entry = entry.file_name().unwrap().to_str().unwrap(),
                             width = max_nlink_width,
                             owner_width = max_owner_width,
                             group_width = max_group_width,
@@ -155,26 +191,31 @@ fn ls(args: Args) -> Result<String> {
         }
 
         #[cfg(not(unix))]
-        Ok(entries.join("\n"))
+        Ok(entries_paths.join("\n"))
     } else {
         // Iterate over the terminal width
         let terminal_width = size()?.0 as usize;
 
         // The max entry width is the longest string in the entry set
-        let max_entry_width = entries.iter().map(|entry| entry.len()).max().unwrap_or(0) + 2; // +2 for padding
+        let max_entry_width = entries_strs
+            .iter()
+            .map(|entry| entry.len())
+            .max()
+            .unwrap_or(0)
+            + 2; // +2 for padding
 
         // The number of columns is the maximum extent to which we can it values, given the longest entry.
         let columns = terminal_width / max_entry_width;
 
         // We want the longest columns to be on the left side, so we round up by adding columns - 1.
-        let rows = (entries.len() + columns - 1) / columns;
+        let rows = (entries_paths.len() + columns - 1) / columns;
 
         let mut output = String::new();
 
         // Now, build the output column by column in alphabetical order (just like coreutils ls does it.)
         for row in 0..rows {
             for column in 0..columns {
-                if let Some(entry) = entries.get(row + column * rows) {
+                if let Some(entry) = entries_strs.get(row + column * rows) {
                     output += entry;
                     // Add spaces only if we're *not* in the end column.
                     if column != columns - 1 {
